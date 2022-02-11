4609cd4ee122a595fb03381985c0075726c06364
John Maraist <from-github@maraist.org>
2022-02-11 00:18:19 -0600

    Various improvement to printing, some sample passing and failing tests

--- src/Test/TLT.hs
+++ src/Test/TLT.hs
@@ -2,7 +2,7 @@
 Module      : TLT
 Description : Testing in a monad transformer layer
 Copyright   : (c) John Maraist, 2022
-License     : AllRightsReserved
+License     : GPL3
 Maintainer  : haskell-tlt@maraist.org
 Stability   : experimental
 Portability : POSIX
@@ -15,63 +15,128 @@ language governing permissions and limitations under the License.
 
 -}
 
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Test.TLT (
-  TestFail, Assertion,
-  TLT, tlt,
+  -- * A monad transformer for housing a test process
+  TLT, tlt, -- MonadTLT,
+  -- * Writing tests
+  Assertion,
+  -- ** `TLT` commands
   (~:), (~::), (~::-),
+  -- ** Assertions
   (!==),  (!/=),  (!<),  (!>),  (!<=),  (!>=),
-  (!==-), (!/=-), (!<-), (!>-), (!<=-), (!>=-)
+  (!==-), (!/=-), (!<-), (!>-), (!<=-), (!>=-),
+  emptyP, empty,
+  -- ** Building new assertions
+  liftAssertion2Pure, assertion2PtoM, liftAssertion2M,
+  liftAssertionPure, assertionPtoM, liftAssertionM
   ) where
 
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State.Lazy
+import System.Console.ANSI
+import System.Exit
 
 -- * Results of tests
 
 -- |Reasons why a test might fail
 data TestFail = Asserted String | Erred
 
--- |An assertion is a computation in a `TLT` monad which return a list
--- of zero of more reasons for the failure of the assertion.  A
--- successful computation returns no reasons for failure.
+formatFail :: TestFail -> String
+formatFail (Asserted s) = s
+formatFail (Erred) = "Assertion called error"
+
+-- |An assertion is a computation (typically in the monad wrapped by
+-- `TLT`) which returns a list of zero of more reasons for the failure
+-- of the assertion.  A successful computation returns an empty list:
+-- no reasons for failure, hence success.
 type Assertion m = m [TestFail]
 
 -- |Hierarchical structure holding the result of running tests,
 -- possibly grouped into tests.
 data TestResult = Test String [TestFail]
-                | Group String [TestResult]
+                | Group String Int [TestResult]
+
+-- |Return the number of failed tests reported in a `TestResult`.
+failCount :: TestResult -> Int
+failCount (Test _ []) = 0
+failCount (Test _ _) = 1
+failCount (Group _ n _) = n
+
+totalFailCount :: [TestResult] -> Int
+totalFailCount = foldr (\tr n -> n + failCount tr) 0
 
 -- |Report the results of tests.
 report :: [TestResult] -> IO ()
-report trs = report' "" trs
+report trs = let fails = totalFailCount trs
+             in do report' "" trs
+                   if fails > 0
+                     then do putStrLn $
+                               "Found " ++ show fails ++ " error"
+                               ++ (if fails > 1 then "s" else "")
+                               ++ "; exiting"
+                             exitFailure
+                     else return()
   where report' ind trs = forM_ trs $ \ tr -> do
           case tr of
-            Test s r -> putStrLn $ ind ++ "- " ++ s ++ ": " ++
-                                     if null r then "passed" else "FAIL"
-            Group s trs' -> do
-              putStrLn $ ind ++ "- " ++ s ++ ":"
+            Test s r -> do
+              putStr $ ind ++ "- " ++ s ++ ": "
+              case r of
+                [] -> do
+                  greenPass
+                  putStrLn ""
+                x : [] -> do
+                  redFail
+                  putStrLn $ " " ++ formatFail x
+                _ -> do
+                  redFail
+                  putStrLn ":"
+                  forM_ r $ \ f -> putStrLn $ ind ++ "- " ++ formatFail f
+            Group s _ trs' -> do
+              putStrLn $ ind ++ "  - " ++ s ++ ":"
               report' ("  " ++ ind) trs'
+
+greenPass = do
+  setSGR [
+    SetColor Foreground Vivid Blue
+    ]
+  putStr "Pass"
+  setSGR [
+    SetColor Foreground Vivid Black
+    ]
+
+redFail = do
+  setSGR [
+    SetColor Foreground Vivid Red,
+    SetConsoleIntensity BoldIntensity
+    ]
+  putStr "FAIL"
+  setSGR [
+    SetColor Foreground Vivid Black,
+    SetConsoleIntensity NormalIntensity
+    ]
 
 -- |Accumulator for test results, in the style of a simplified Huet's
 -- zipper which only ever adds to the end of the structure.
-data TRBuf = Buf TRBuf String [TestResult] | Top [TestResult]
+data TRBuf = Buf TRBuf Int String [TestResult] | Top Int [TestResult]
 
 -- |Add a single test result to a `TRBuf`.
 addResult :: TRBuf -> TestResult -> TRBuf
-addResult (Top trs) tr = Top $ tr : trs
-addResult (Buf up s trs) tr = Buf up s $ tr : trs
+addResult (Top n trs) tr = Top (n + failCount tr) $ tr : trs
+addResult (Buf up n s trs) tr = Buf up (n + failCount tr) s $ tr : trs
 
 -- |Convert a `TRBuf` into a list of top-down `TestResult`s.
 closeTRBuf :: TRBuf -> [TestResult]
-closeTRBuf (Top ts) = reverse ts
-closeTRBuf (Buf acc gname gtrs) =
-  closeTRBuf $ closeWith acc $ Group gname $ reverse gtrs
-  where closeWith (Top ts) g = Top $ g : ts
-        closeWith (Buf acc gn gtrs) g = Buf acc gn $ g : gtrs
-
--- * The monad transformer in which test processes live
+closeTRBuf (Top _ ts) = reverse ts
+closeTRBuf (Buf acc count gname gtrs) =
+  closeTRBuf $ closeWith acc $ Group gname count $ reverse gtrs
+  where closeWith (Top n ts) g = Top (n + count) $ g : ts
+        closeWith (Buf acc n gn gtrs) g = Buf acc (n + count) gn $ g : gtrs
 
 -- |Monad transformer for TLT tests.  This layer stores the results
 -- from tests as they are executed.
@@ -114,30 +179,42 @@ instance MonadIO m => MonadIO (TLT m) where
 -- results.
 tlt :: MonadIO m => TLT m r -> m ()
 tlt (TLT t) = do
-  (_, resultsBuf) <- runStateT t $ Top []
+  liftIO $ putStrLn "Running tests:"
+  (_, resultsBuf) <- runStateT t $ Top 0 []
   liftIO $ report $ closeTRBuf resultsBuf
+
+{-  Does not work now, but not important to fix immediately.
+
+-- |Allowing the `TLT` layer to be wrapped by the layers it tests,
+-- instead of `TLT` wrapping them.
+
+class Monad m => MonadTLT m where
+  liftTLT :: (forall m0 . Monad m0 => TLT m0 a) -> m a
+
+instance (MonadTrans m, MonadTLT m0, Monad (m m0)) => MonadTLT (m m0) where
+  liftTLT = lift . liftTLT
+-}
 
 -- * Specifying individual tests
 
-infix 0 ~:
-infix 1 ~::,  !==,  !/=,  !<,  !>,  !<=,  !>=
-infix 1 ~::-, !==-, !/=-, !<-, !>-, !<=-, !>=-
+infix 0 ~:, ~::, ~::-
 
--- |Name and test an assertion.
+-- |Name and perform a test of an `Assertion`.
 (~:) :: Monad m => String -> Assertion m -> TLT m ()
 s ~: a = TLT $ do
   oldState <- get
   assessment <- lift a
   put $ addResult oldState $ Test s assessment
 
--- |Name and test a boolean value.
+-- |Name and perform a test of a (pure) boolean value.
 (~::-) :: Monad m => String -> Bool -> TLT m ()
 s ~::- b = TLT $ do
   oldState <- get
   put $ addResult oldState $ Test s $
     if b then [] else [Asserted $ "Expected True but got False"]
 
--- |Name and test a boolean value.
+-- |Name and perform a test of a boolean value returned by a
+-- computation in the wrapped monad @m@.
 (~::) :: Monad m => String -> m Bool -> TLT m ()
 s ~:: bM = TLT $ do
   b <- lift bM
@@ -145,77 +222,127 @@ s ~:: bM = TLT $ do
   put $ addResult oldState $ Test s $
     if b then [] else [Asserted $ "Expected True but got False"]
 
+infix 1 !==,  !/=,  !<,  !>,  !<=,  !>=
+infix 1 !==-, !/=-, !<-, !>-, !<=-, !>=-
+
+-- |Transform a binary function on an expected and an actual value
+-- (plus a binary generator of a failure message) into an `Assertion`
+-- for a pure given actual value.
+liftAssertion2Pure ::
+  (Monad m) => (a -> a -> Bool) -> (a -> a -> String) -> a -> a -> Assertion m
+liftAssertion2Pure tester explainer exp actual = return $
+  if (tester exp actual) then [] else [Asserted $ explainer exp actual]
+
+-- |Given an `Assertion` for two pure values (expected and actual),
+-- lift it to an `Assertion` expecting the actual value to be returned
+-- from a computation.
+assertion2PtoM ::
+  (Monad m) => (a -> a -> Assertion m) -> a -> m a -> Assertion m
+assertion2PtoM pa exp actualM = do actual <- actualM
+                                   pa exp actual
+
+-- |Transform a binary function on expected and actual values (plus
+-- a generator of a failure message) into an `Assertion` where the
+-- actual value is to be returned from a subcomputation.
+liftAssertion2M ::
+  (Monad m) => (a -> a -> Bool) -> (a -> a -> String) -> a -> m a -> Assertion m
+liftAssertion2M tester explainer exp actualM =
+  let assertPure = liftAssertion2Pure tester explainer exp
+  in do actual <- actualM
+        assertPure actual
+
 -- |Assert that two values are equal.
 (!==-) :: (Monad m, Eq a, Show a) => a -> a -> Assertion m
-exp !==- actual = return $
-  if (exp == actual) then []
-  else [Asserted $ "Expected " ++ show exp ++ " but got " ++ show actual]
+(!==-) = liftAssertion2Pure (==) $
+  \ exp actual -> "Expected " ++ show exp ++ " but got " ++ show actual
 
 -- |Assert that a calculated value is as expected.
 (!==) :: (Monad m, Eq a, Show a) => a -> m a -> Assertion m
-exp !== actualM = do actual <- actualM
-                     exp !==- actual
+(!==) = assertion2PtoM (!==-)
 
 -- |Assert that two values are not equal.
 (!/=-) :: (Monad m, Eq a, Show a) => a -> a -> Assertion m
-exp !/=- actual = return $
-  if (exp /= actual) then []
-  else [Asserted $ "Expected different than " ++ show exp ++ " but got " ++ show actual]
+(!/=-) = liftAssertion2Pure (/=) $
+  \ exp actual ->
+    "Expected other than " ++ show exp ++ " but got " ++ show actual
 
 -- |Assert that a calculated value differs from some known value.
 (!/=) :: (Monad m, Eq a, Show a) => a -> m a -> Assertion m
-exp !/= actualM = do
-  actual <- actualM
-  exp !/=- actual
+(!/=) = assertion2PtoM (!/=-)
 
 -- |Assert that a given boundary is strictly less than some value.
 (!<-) :: (Monad m, Ord a, Show a) => a -> a -> Assertion m
-exp !<- actual = return $
-  if (exp < actual) then []
-  else [Asserted $ "Lower bound (open) is " ++ show exp ++ " but got " ++ show actual]
+(!<-) = liftAssertion2Pure (<) $
+  \ exp actual ->
+    "Lower bound (open) is " ++ show exp ++ " but got " ++ show actual
 
 -- |Assert that a given, constant boundary is strictly less than some
 -- calculated value.
 (!<) :: (Monad m, Ord a, Show a) => a -> m a -> Assertion m
-exp !< actualM = do
-  actual <- actualM
-  exp !<- actual
+(!<) = assertion2PtoM (!<-)
 
 -- |Assert that a given boundary is strictly less than some value.
 (!>-) :: (Monad m, Ord a, Show a) => a -> a -> Assertion m
-exp !>- actual = return $
-  if (exp > actual) then []
-  else [Asserted $ "Upper bound (open) is " ++ show exp ++ " but got " ++ show actual]
+(!>-) = liftAssertion2Pure (>) $
+  \ exp actual ->
+    "Upper bound (open) is " ++ show exp ++ " but got " ++ show actual
 
 -- |Assert that a given, constant boundary is strictly less than some
 -- calculated value.
 (!>) :: (Monad m, Ord a, Show a) => a -> m a -> Assertion m
-exp !> actualM = do
-  actual <- actualM
-  exp !<- actual
+(!>) = assertion2PtoM (!>-)
 
 -- |Assert that a given boundary is strictly less than some value.
 (!<=-) :: (Monad m, Ord a, Show a) => a -> a -> Assertion m
-exp !<=- actual = return $
-  if (exp <= actual) then []
-  else [Asserted $ "Lower bound (closed) is " ++ show exp ++ " but got " ++ show actual]
+(!<=-) = liftAssertion2Pure (<=) $
+  \ exp actual ->
+    "Lower bound (closed) is " ++ show exp ++ " but got " ++ show actual
 
 -- |Assert that a given, constant boundary is strictly less than some
 -- calculated value.
 (!<=) :: (Monad m, Ord a, Show a) => a -> m a -> Assertion m
-exp !<= actualM = do
-  actual <- actualM
-  exp !<- actual
+(!<=) = assertion2PtoM (!<=-)
 
 -- |Assert that a given boundary is strictly less than some value.
 (!>=-) :: (Monad m, Ord a, Show a) => a -> a -> Assertion m
-exp !>=- actual = return $
-  if (exp >= actual) then []
-  else [Asserted $ "Upper bound (closed) is " ++ show exp ++ " but got " ++ show actual]
+(!>=-) = liftAssertion2Pure (>=) $
+  \ exp actual ->
+    "Upper bound (closed) is " ++ show exp ++ " but got " ++ show actual
 
 -- |Assert that a given, constant boundary is strictly less than some
 -- calculated value.
 (!>=) :: (Monad m, Ord a, Show a) => a -> m a -> Assertion m
-exp !>= actualM = do
-  actual <- actualM
-  exp !<- actual
+(!>=) = assertion2PtoM (!>=-)
+
+-- |Transform a unary function on a value (plus a generator of a
+-- failure message) into a unary `Assertion` function for a pure given
+-- actual value.
+liftAssertionPure ::
+  (Monad m) => (a -> Bool) -> (a -> String) -> a -> Assertion m
+liftAssertionPure tester explainer actual = return $
+  if (tester actual) then [] else [Asserted $ explainer actual]
+
+-- |Given an `Assertion` for a pure (actual) value, lift it to an
+-- `Assertion` expecting the value to be returned from a computation.
+assertionPtoM :: (Monad m) => (a -> Assertion m) -> m a -> Assertion m
+assertionPtoM pa actualM = do actual <- actualM
+                              pa actual
+
+-- |Transform a unary function on an actual value (plus a generator
+-- of a failure message) into an `Assertion` where the value is to be
+-- returned from a subcomputation.
+liftAssertionM ::
+  (Monad m) => (a -> Bool) -> (a -> String) -> m a -> Assertion m
+liftAssertionM tester explainer actualM =
+  let assertPure = liftAssertionPure tester explainer
+  in do actual <- actualM
+        assertPure actual
+
+-- |Assert that a pure foldable structure (such as a list) is empty.
+emptyP :: (Monad m, Foldable t) => t a -> Assertion m
+emptyP = liftAssertionPure null
+           (\ _ -> "Expected empty structure but got non-empty")
+
+-- |Assert that a structure returned from a computation is empty.
+empty :: (Monad m, Foldable t) => m (t a) -> Assertion m
+empty = assertionPtoM emptyP
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+import Test.TLT
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  tlt test
+  {- runJTMST $ do
+    counts <- testEx1
+    liftIO $ putStrLn $ show counts -}
+  putStrLn "Test run completed"
+
+test :: Monad m => TLT m ()
+test = do
+  "True passes" ~::- True
+  "2 is 3 as single Bool" ~::- 2 == 3
+  "2 is 2 as single Bool" ~::- 2 == 2
+  "2 is 3 as pure assertion" ~: 2 !==- 3
+  "2 is 2 as pure assertion" ~: 2 !==- 2
+  "2 is 3 as result" ~: 2 !== return 3
+  "2 is 2 as result" ~: 2 !== return 2
+  "2 not 3 as pure assertion" ~: 2 !/=- 3
+  "2 not 2 as pure assertion" ~: 2 !/=- 2
+  "2 not 3 as result" ~: 2 !/= return 3
+  "2 not 2 as result" ~: 2 !/= return 2
